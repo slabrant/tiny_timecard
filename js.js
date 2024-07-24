07cdefd5bc7edebea315fa1d0faf6ec1ab1e1f74
slabrant <71408627+slabrant@users.noreply.github.com>
2024-07-24 10:02:32 -0500

    Initial commit
    Began creating this tiny timecard.
    Update timecard to allow date changing and download as csv. Got carried away and did everything on my little list and implemented back and next buttons next to the day picker. I think it's done - except that the download can potentially have the dates in the wrong order.

---
+++ js.js
@@ -0,0 +1,152 @@
+const dateFormat = new Intl.DateTimeFormat('en-CA', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+});
+const timeFormat = new Intl.DateTimeFormat('en-CA', {
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false,
+});
+
+document.getElementById('backDayButton').addEventListener('click', (e) => {
+    updateDayByAmount(-1);
+})
+
+document.getElementById('dateInput').addEventListener('change', (e) => {
+    date = e.target.value;
+    addRowsForDay(date);
+});
+
+document.getElementById('nextDayButton').addEventListener('click', (e) => {
+    updateDayByAmount(1);
+})
+
+document.getElementById('addButton').addEventListener('click', (e) => {
+    const now = new Date;
+    const time = timeFormat.format(now);
+
+    let days = JSON.parse(localStorage.getItem('days'));
+    let newEntryId = days[date].length;
+    if (days[date][newEntryId-1]?.start === time) {
+        return;
+    }
+    if (days[date][newEntryId-1] && days[date][newEntryId-1].stop === '') {
+        days[date][newEntryId-1].stop = time;
+    }
+
+    days[date].push({
+        id: newEntryId,
+        start: time,
+        stop: '',
+        notes: ''
+    });
+
+    localStorage.setItem('days', JSON.stringify(days));
+
+    location.reload()
+    // addRow({start: time});
+});
+
+document.getElementById('saveButton').addEventListener('click', (e) => {
+    saveRows();
+});
+
+document.getElementById('downloadButton').addEventListener('click', (e) => {
+    let days = JSON.parse(localStorage.getItem('days'));
+
+    let csv = 'data:text/csv;charset=utf-8,'
+    for (let date in days) {
+        csv += date + '\n'
+        for (let entryKey in days[date]) {
+            csv += `${days[date][entryKey]['start']},${days[date][entryKey]['stop']},${days[date][entryKey]['notes']}\n`;
+        }
+    }
+    const now = new Date;
+    const todaysDate = dateFormat.format(now);
+    const time = timeFormat.format(now);
+    const anchor = document.createElement('a');
+    anchor.href = csv;
+    anchor.download = `tiny_time_${todaysDate}_${time}.csv`;
+    anchor.click();
+});
+
+document.addEventListener('keydown', (e) => {
+    if (e.ctrlKey && e.key.toLowerCase() === 's') {
+        e.preventDefault();
+        saveRows();
+    }
+})
+
+const addRow = ({id = -1, start = '', stop = '', notes = ''}) => {
+    let rowTemplate = document.getElementById('rowTemplate');
+
+    let newRow = rowTemplate.cloneNode(true);
+    newRow.id = 'row_' + id;
+    document.getElementById('rows').appendChild(newRow);
+
+    newRow.querySelector('.start').value = start;
+    newRow.querySelector('.stop').value = stop;
+    newRow.querySelector('.notes').value = notes;
+    newRow.hidden = false;
+
+    newRow.querySelector('.remove').addEventListener('click', (e) => {
+        newRow.remove();
+        days[date] = days[date].filter((entry) => entry.id !== id)
+        localStorage.setItem('days', JSON.stringify(days));
+    });
+};
+
+const saveRows = () => {
+    let days = JSON.parse(localStorage.getItem('days'));
+
+    let rowElements = document.getElementById('rows').children;
+    
+    let newEntries = [];
+    let idCount = 0;
+    Array.from(rowElements).map(rowElement => {
+        newEntries.push({
+            id: idCount,
+            start: rowElement.querySelector('.start').value,
+            stop: rowElement.querySelector('.stop').value,
+            notes: rowElement.querySelector('.notes').value,
+        });
+        idCount++;
+    });
+    days[date] = newEntries;
+
+    localStorage.setItem('days', JSON.stringify(days));
+}
+
+const addRowsForDay = (date) => {
+    localStorage.setItem('date', date);
+
+    let days = JSON.parse(localStorage.getItem('days'));
+    if (!days) {
+        days = {};
+    };
+    if (!days?.[date]) days[date] = [];
+    localStorage.setItem('days', JSON.stringify(days));
+
+    let rowsElement = document.getElementById('rows');
+    rowsElement.innerHTML = '';
+
+    days[date].map((entry) => {
+        addRow(entry);
+    })
+}
+
+const updateDayByAmount = (amount) => {
+    let datePieces = date.split('-');
+    let changedDate = new Date(datePieces[0], datePieces[1] - 1, +datePieces[2] + +amount)
+    date = dateFormat.format(changedDate);
+    document.getElementById('dateInput').value = date;
+    addRowsForDay(date);
+}
+
+const now = new Date;
+let date = localStorage.getItem('date');
+if (!date) date = dateFormat.format(now);
+document.getElementById('dateInput').value = date;
+
+addRowsForDay(date);
